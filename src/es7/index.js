911503b9cff0082f619b526b03f07870e3ec6983
pikyr <pikytdv@gmail.com>
2022-05-14 14:21:05 -0300

    Fin de clase 06 - Includes y potencias en ES7

---
+++ src/es7/index.js
@@ -0,0 +1,20 @@
+/**
+ * CLASE 06
+ */
+
+// ** INCLUDES
+
+let numbers = [1, 11, 22, 3, 33, 88, 7];
+
+if (numbers.includes(11)) {
+  console.log("Hay un 11 en la lista");
+} else {
+  console.log("La lista no contiene un 11");
+}
+
+// ** POTENCIAS
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
