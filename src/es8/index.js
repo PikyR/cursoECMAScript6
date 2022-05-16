6e1784acd2b88045cbe2944f52356c75d3c00bb4
pikyr <pikytdv@gmail.com>
2022-05-15 21:26:15 -0300

    Fin de clase 08 - Async y await

--- src/es8/index.js
+++ src/es8/index.js
@@ -30,3 +30,37 @@ console.log(valores.length); // 3
 const texto = "Soy un texto";
 console.log(texto.padStart(17, "Hola "));
 console.log(texto.padEnd(15, " :)"));
+/* FIN DE CLASE 07 */
+
+/**
+ * CLASE 08
+ */
+// ** ASYNC - AWAIT 
+
+const nuevaFuncion = () => {
+  return new Promise((resolve, reject) => {
+    // if ternario
+    (true)
+      ? setTimeout (() => resolve("Hola asincronismo!"), 2000)
+      : reject(new Error("Test error!"))
+  })
+};
+
+const helloAsync = async () => {
+  const saludo = await nuevaFuncion();
+  console.log(saludo);
+}
+
+helloAsync();
+
+
+const otraFuncion = async () => {
+  try {
+    const hello = await nuevaFuncion();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+otraFuncion();
